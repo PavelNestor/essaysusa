f718c72816fc712dc26958315a19389a9764cfec
PavelNestor <pavelnesterets82@gmail.com>
2019-10-29 02:24:56 +0200

    finish main-page

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,44 @@
+"use strict";
+import AOS from "AOS";
+
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+// handle open menu
+(function() {
+  var menuOpen = $('#menu-open');
+  var menuClose = $('#menu-close');
+  var menuContent = $('#menu');
+
+  const onToogleMenu = () => {
+    menuContent.classList.toggle('menu_active');
+  };
+
+  menuOpen.addEventListener("click", onToogleMenu);
+  menuClose.addEventListener("click", onToogleMenu);
+})();
+
+
+//Navbar scrollBehavior
+let prevScrollpos = 0;
+
+const handleScrollBehavior = () => {
+  const navbar = $("#navbar");
+  let currentScrollPos = window.pageYOffset;
+
+  if (currentScrollPos > 200 && prevScrollpos > currentScrollPos) {
+    navbar.classList.add('navbar_active');
+  } else {
+    navbar.classList.remove('navbar_active');
+  }
+
+  prevScrollpos = currentScrollPos;
+};
+
+window.addEventListener('scroll', handleScrollBehavior);
+
+
+// handle tabs
 const select = document.querySelector.bind(document);
 const selectAll = document.querySelectorAll.bind(document);
 
@@ -15,3 +56,6 @@ const handleLinkClick = index => {
 
 tabLinks.forEach((tabLink, index) => tabLink.addEventListener('click', () => handleLinkClick(index)));
 tabLinksDefault.click();
+
+// animation on scroll
+AOS.init();
